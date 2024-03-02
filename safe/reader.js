df87361ee541c5f1d54a870355eb79875fd89fc9
fuyunlong <1273179133@qq.com>
2024-03-02 22:38:00 +0800

    feat: some example

---
+++ safe/reader.js
@@ -0,0 +1,54 @@
+const puppeteer = require("puppeteer");
+const { shuffle, random } = require("lodash");
+
+//延迟函数
+const sleep = (time) => {
+  return new Promise((r, j) => {
+    setTimeout(() => {
+      r(time);
+    }, time);
+  });
+};
+let sum = 1;
+
+(async () => {
+  async function reader(allArticle) {
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+      args: ["--start-maximized"],
+    });
+
+    // 1. 启动一个Chrome浏览器实例
+    const page = await browser.newPage(); // 创建一个新的标签页
+
+    const maxNumber = Number(process.argv[2])
+      ? Number(process.argv[2])
+      : 60;
+
+    // 遍历刷新
+    for (let i = 0; i < 1; i++) {
+      const time = 5000;
+      const url = allArticle[i];
+      console.log(url, i);
+      console.log(`已完成${i + 1}篇`);
+      await page.goto(url); // 导航到指定的网页
+      await sleep(time);
+      i && console.clear();
+    }
+    await browser.close(); // 关闭浏览器实例
+  }
+
+  const allArticle = [
+    // "https://juejin.cn/post/7336656747284004900",
+    // "https://juejin.cn/post/7337170233604603967",
+    // "https://juejin.cn/post/7338742634168139788",
+    "https://juejin.cn/post/7340152660224720908",
+  ];
+  reader(allArticle);
+  const timer = setInterval(async () => {
+    await reader(allArticle);
+    console.log(`完成${sum++}轮刷新`);
+    if (sum > 80) clearInterval(timer);
+  }, 60 * 1000);
+})();
